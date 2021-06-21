5989edf38dc126ec79d02cbd2e21b6bdb5082613
Nicolas-concina <nicoconcina@gmail.com>
2021-06-21 16:59:19 +0200

    troisieme exo resolu

--- index.js
+++ index.js
@@ -24,3 +24,35 @@ const palindrome = (str) => {
     }
 }
 console.log(palindrome("kayaK"));
+
+/*Troisieme Test Travelling Snails */
+
+
+
+const heights = [10,5,8,3,25,7,9];  // point de depart
+const dSpeed = [2,3,2,4,5,3,2];  // distance ascendant parcouru
+const nSpeed = [1,2,1,3,1,2,0]; // distance descendant parcouru
+const tempsArriver = [];
+
+
+
+for (let i = 0; i < heights.length; i++) {
+    let tempsArrive =0;
+   while (heights[i]>0) {
+       heights[i]=heights[i]-dSpeed[i]+nSpeed[i];
+      tempsArrive++;
+   }
+   tempsArriver.push(tempsArrive);
+   tempsArrive=0;
+}
+console.log(tempsArriver);
+ const ordreArriver = tempsArriver.sort(compare);
+console.log(ordreArriver);
+
+
+
+
+// prendre le premiere iterateur de chaque array pour le soustraire la valeur de dSpeed puis lui ajouter la valeur de nSpeed.
+// on compte le nbr de fois qu il boucle pour arriver a 0 et ont obtient leur nbr d iteration avec sa on a un classement
+// ex: (10 - 2) + 1 tant que heigth >= 0 je recomence si height = 0 je le stock dans temps arriver et  a chaque fois que je recomence j ajoute 1 tempsArriver
+//
